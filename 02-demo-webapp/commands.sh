24e762ab07ff4ec9a5c20bec1e5a7354eb82337b
Nicolas Muller <zepouet@gmail.com>
2016-09-05 15:15:30 +0200

    Update commands.sh

--- 02-demo-webapp/commands.sh
+++ 02-demo-webapp/commands.sh
@@ -17,11 +17,13 @@ docker build --tag="demo/tomcat-data" .
 ## ###############################
 ## RUN DATA CONTAINER
 ## ###############################
-docker run -it --name=hello-app demo/tomcat-data ls /tomcat/webapps
+docker run -it --name=tdata demo/tomcat-data bash
+exit
 
 ## ##################################### ##
 ## RUN TOMCAT CONTAINER WITH ASSOCIATION ##
 ## ##################################### ##
-docker run --name=tomcat81 -d --volumes-from hello-app tutum/tomcat:8.0
-docker run --name=tomcat82 -d -p 8080 --volumes-from hello-app tutum/tomcat:8.0
-docker run --name=tomcat83 -d -p 8080:8080 --volumes-from hello-app tutum/tomcat:8.0
+docker run --name=tomcat81 -d --volumes-from tdata tutum/tomcat:8.0
+docker run --name=tomcat82 -d -p 8080 --volumes-from tdata tutum/tomcat:8.0
+docker run --name=tomcat83 -d -p 8080:8080 --volumes-from tdata tutum/tomcat:8.0
+
